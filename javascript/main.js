855ca4aeb0a017f97f55bc4713f569403230f083
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-08-22 11:26:44 +0200

    update js code

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,28 @@
-const baseURL = "../produtos.json"
+const baseURL = "../produtos.json";
+const contentTable = document.querySelector(".content tbody");
 
-const response =  fetch(baseURL, {
-  method: "GET",
-  headers: {
-    "Conntent-Type": "application/json",
-  },
-  
-}).then();
+async function request() {
+  try {
+    const response = await fetch(baseURL);
+    return response.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+request().then(function (response) {
+  response.forEach((produto) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td>${produto.id}</td>
+        <td>${produto.name}</td>
+        <td>${produto.brand}</td>
+        <td>${produto.qtd}</td>
+        <td>
+          <button>edit</button>
+          <button>remove</button>
+        </td>
+        `;
+    contentTable.appendChild(tr);
+  });
+});
