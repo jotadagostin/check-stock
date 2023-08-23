82c2f7ec9b72c89e4b4c36d858b0cf50403fd7ca
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-08-23 11:29:58 +0200

    js code

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,10 @@
-const baseURL = "../produtos.json";
+const baseURL = "https://03d0-2804-14d-5c32-8e78-400-d750-f490-966a-ngrok-free.app/";
 const contentTable = document.querySelector(".content tbody");
 
 async function request() {
   try {
-    const response = await fetch(baseURL);
+    const response = await fetch(baseURL + "cadastroProdutos");
+    console.log(response.json());
     return response.json();
   } catch (e) {
     console.error(e);
