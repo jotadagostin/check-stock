c89f6e97d1a0a371065e909bfb282a8abc6740a4
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-08-24 11:52:20 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,9 @@
-const baseURL = "https://03d0-2804-14d-5c32-8e78-400-d750-f490-966a-ngrok-free.app/";
+const baseURL = "http://localhost:3000";
 const contentTable = document.querySelector(".content tbody");
 
 async function request() {
   try {
     const response = await fetch(baseURL + "cadastroProdutos");
-    console.log(response.json());
     return response.json();
   } catch (e) {
     console.error(e);
@@ -12,6 +11,7 @@ async function request() {
 }
 
 request().then(function (response) {
+  console.log(response);
   response.forEach((produto) => {
     const tr = document.createElement("tr");
     tr.innerHTML = `
